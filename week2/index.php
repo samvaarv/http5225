e4998c7cdb914639452f38234902f83355a1284a
Samvaarv Shrestha <samvaarv.vid@gmail.com>
2024-05-15 20:13:26 -0400

    added img and echo other html elements

--- week2/index.php
+++ week2/index.php
@@ -1,3 +1,16 @@
-<?php
-echo "Hello World!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>    
+    <?php
+    echo '<h1>PHP Output and Variables</h1>';
+    echo '<p>Learning how to work with variables in PHP</p>';
+    ?>
+    
+    <img src="https://plus.unsplash.com/premium_photo-1675440682547-d3be56f91621?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="50%" alt="">
+</body>
+</html>
